32c1f5734acd3dde4b01f5a80c63fc0d8e2b183b
saifullah199 <mdkhalidsaifullah199@gmail.com>
2024-04-09 08:03:04 +0600

    provider added

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,9 +1,70 @@
+import { useContext } from "react";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../AuthProvider/AuthProvider";
+import { useForm } from "react-hook-form";
 
 
 const Register = () => {
+
+  const {createUser} = useContext(AuthContext)
+  console.log(createUser)
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm()
+
+  const onSubmit = (data) => {
+    console.log(data)
+  }
     return (
         <div>
-            this is Register
+            <div className="hero min-h-screen bg-base-200">
+  <div className="hero-content flex-col ">
+    <div className="text-center lg:text-left">
+      <h1 className="text-5xl font-bold">Register Now!</h1>
+      
+    </div>
+    <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+      <form  onSubmit={handleSubmit(onSubmit)} className="card-body">
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Full Name</span>
+          </label>
+          <input type="text" name="name" placeholder="Your Name" className="input input-bordered"  
+          {...register("fullName", { required: true })} />
+           {errors.fullName && <span>This field is required</span>}
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Photo URL</span>
+          </label>
+          <input type="text" name="photoUrl" placeholder="Photo URL" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Email</span>
+          </label>
+          <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+        </div>
+        <div className="form-control">
+          <label className="label">
+            <span className="label-text">Password</span>
+          </label>
+          <input type="password" placeholder="password" className="input input-bordered" required />
+          
+        </div>
+        <div className="form-control mt-6">
+          <button className="btn btn-primary">Register</button>
+        </div>
+        <div>
+            <p>Already have an account? <Link className="text-primary" to="/login">Login</Link></p>
+        </div>
+      </form>
+    </div>
+  </div>
+</div>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -11,6 +11,7 @@ import Home from './Pages/Home';
 import Login from './Pages/Login';
 import Register from './Pages/Register';
 import UpdateProfile from './Pages/UpdateProfile';
+import AuthProvider from './AuthProvider/AuthProvider';
 
 const router = createBrowserRouter([
   {
@@ -40,6 +41,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
   </React.StrictMode>,
 )
